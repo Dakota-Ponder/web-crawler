e5bc154892d08170e1ea6f4c66049207c56efffa
Dakota-Ponder <dponder12358@gmail.com>
2023-12-08 12:10:01 -0600

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,36 @@
+const { JSDOM } = require('jsdom')
+
+// function that grabs URLs from the HTML
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative url 
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            }
+        } else {
+            // absolute url 
+             try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+        
+    }
+    console.log(urls)
+    return urls
+}
+
+
+// function that takes a url as input and makes it all uniform 
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
 
@@ -10,11 +43,8 @@ function normalizeURL(urlString) {
     return hostPath;
 }
 
-function getURLsFromHTML(htmlBody, baseURL) {
-    
-}
-
 // stub the function
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
